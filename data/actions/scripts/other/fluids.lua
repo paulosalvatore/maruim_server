d032512505f0f69e41d1f93c28b86130e14deb90
paulosalvatore <paulo.salvatore@hotmail.com.br>
2014-12-31 04:37:49 -0200

    Update Fluids

--- data/actions/scripts/other/fluids.lua
+++ data/actions/scripts/other/fluids.lua
@@ -9,9 +9,6 @@ poison:setParameter(CONDITION_PARAM_STARTVALUE, -5)
 poison:setParameter(CONDITION_PARAM_TICKINTERVAL, 4000)
 poison:setParameter(CONDITION_PARAM_FORCEUPDATE, true)
 
-local fluidType = {3, 4, 5, 7, 10, 11, 13, 15, 19}
-local fluidMessage = {"Aah...", "Urgh!", "Mmmh.", "Aaaah...", "Aaaah...", "Urgh!", "Urgh!", "Aah...", "Urgh!"}
-
 function onUse(player, item, fromPosition, itemEx, toPosition, isHotkey)
 	local itemExType = ItemType(itemEx.itemid)
 	if itemExType and itemExType:isFluidContainer() then
@@ -30,7 +27,6 @@ function onUse(player, item, fromPosition, itemEx, toPosition, isHotkey)
 		if item.type == 0 then
 			player:sendTextMessage(MESSAGE_STATUS_SMALL, "It is empty.")
 		elseif itemEx.uid == player:getId() then
-			Item(item.uid):transform(item.itemid, 0)
 			if item.type == 3 or item.type == 15 then
 				player:addCondition(drunk)
 			elseif item.type == 4 then
@@ -42,13 +38,12 @@ function onUse(player, item, fromPosition, itemEx, toPosition, isHotkey)
 				player:addHealth(60)
 				fromPosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
 			end
-			for i = 0, #fluidType do
-				if item.type == fluidType[i] then
-					player:say(fluidMessage[i], TALKTYPE_MONSTER_SAY)
-					return true
-				end
+			if fluids[item.type] ~= nil then
+				player:say(fluids[item.type].message, TALKTYPE_MONSTER_SAY)
+			else
+				player:say("Gulp.", TALKTYPE_MONSTER_SAY)
 			end
-			player:say("Gulp.", TALKTYPE_MONSTER_SAY)
+			Item(item.uid):transform(item.itemid, 0)
 		else
 			Game.createItem(2016, item.type, toPosition):decay()
 			Item(item.uid):transform(item.itemid, 0)
