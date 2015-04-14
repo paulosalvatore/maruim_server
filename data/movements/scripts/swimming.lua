a842b18522241f7ea889201f09beda382def9fb6
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-04-13 23:13:01 -0300

    Script de natação alterado
    Algumas pequenas modificações para otimizar o código;
    Efeito de splash adicionado;
    Nadar recupera o jogador de todas as condições negativas (debuffs).

--- data/movements/scripts/swimming.lua
+++ data/movements/scripts/swimming.lua
@@ -1,20 +1,25 @@
+local conditions = {CONDITION_POISON, CONDITION_FIRE, CONDITION_ENERGY, CONDITION_PARALYZE, CONDITION_DRUNK, CONDITION_DROWN, CONDITION_FREEZING, CONDITION_DAZZLED, CONDITION_CURSED, CONDITION_BLEEDING}
 local outfit = {lookType = 267, lookHead = 0, lookBody = 0, lookLegs = 0, lookFeet = 0, lookTypeEx = 0, lookAddons = 0}
-
-function onStepIn(creature, item, position, fromPosition)
-	if not creature:isPlayer() then
+function onStepIn(player, item, position, fromPosition)
+	if not player then
 		return false
 	end
-
-	doSetCreatureOutfit(creature, outfit, -1)
-	return true
+	if(player:getOutfit().lookType ~= outfit.lookType) then
+		player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
+		doSetCreatureOutfit(player, outfit, -1)
+	end
+	for i, v in ipairs(conditions) do
+		if(getCreatureCondition(player, v) == TRUE) then
+			doRemoveCondition(player, v)
+		end
+	end
 end
-
-function onStepOut(creature, item, position, fromPosition)
-	local player = creature:getPlayer()
-	if player == nil then
+function onStepOut(player, item, position, fromPosition)
+	if not player then
 		return false
 	end
-
-	player:removeCondition(CONDITION_OUTFIT)
-	return true
+	local ground_id = player:getPosition():getTile():getGround():getId()
+	if((ground_id < 4620) or (ground_id > 4625)) then
+		player:removeCondition(CONDITION_OUTFIT)
+	end
 end
