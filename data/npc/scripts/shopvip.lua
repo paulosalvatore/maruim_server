233c46f36d9dc2b58f7eb0be88d219d6adafc1e5
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-04-18 01:50:15 -0300

    Diversos NPCs criados
    Diversos NPCs vendedores foram criados e a lista de itens incluída.

--- data/npc/scripts/shopvip.lua
+++ data/npc/scripts/shopvip.lua
@@ -7,30 +7,30 @@ function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
 function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
 function onThink() npcHandler:onThink() end
 function creatureSayCallback(cid, type, msg)
-if(not npcHandler:isFocused(cid)) then return false end
-local talkUser = NPCHANDLER_CONVbehavior == CONVERSATION_DEFAULT and 0 or cid
-local shopWindow = {}
-local moeda = 12401
-local t = {
-	[2195] = {price = 15},
-	[2493] = {price = 25},
-	[2361] = {price = 30},
-	[8851] = {price = 20},
-	[8925] = {price = 30},
-	[2640] = {price = 50},
-	[2494] = {price = 100},
-	[9932] = {price = 50},
-	[2472] = {price = 70},
-	[8931] = {price = 100}
-}
-local onBuy = function(cid, item, subType, amount, ignoreCap, inBackpacks)
-    if t[item] and not doPlayerRemoveItem(cid, moeda, t[item].price) then
-          selfSay("Você não tem "..t[item].price.." "..getItemName(moeda), cid)
-             else
-        doPlayerAddItem(cid, item)
-        selfSay("Aqui está seu item!", cid)
-       end
-    return true
+	if(not npcHandler:isFocused(cid)) then return false end
+	local talkUser = NPCHANDLER_CONVbehavior == CONVERSATION_DEFAULT and 0 or cid
+	local shopWindow = {}
+	local moeda = 12401
+	local t = {
+		[2195] = {price = 15},
+		[2493] = {price = 25},
+		[2361] = {price = 30},
+		[8851] = {price = 20},
+		[8925] = {price = 30},
+		[2640] = {price = 50},
+		[2494] = {price = 100},
+		[9932] = {price = 50},
+		[2472] = {price = 70},
+		[8931] = {price = 100}
+	}
+	local onBuy = function(cid, item, subType, amount, ignoreCap, inBackpacks)
+	if t[item] and not doPlayerRemoveItem(cid, moeda, t[item].price) then
+		selfSay("Você não tem "..t[item].price.." "..getItemName(moeda), cid)
+	else
+		doPlayerAddItem(cid, item)
+		selfSay("Aqui está seu item!", cid)
+	end
+	return true
 end
 if(msgcontains(msg, 'trade') or msgcontains(msg, 'TRADE')) then
 	for var, ret in pairs(t) do
