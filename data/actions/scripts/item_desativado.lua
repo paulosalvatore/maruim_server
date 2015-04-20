d751c6b95b4d0042592c6aa8a2b616ac471c4574
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-04-20 01:50:34 -0300

    Atualização action 'Item Desativado'
    Modificação feita para utilizar o mesmo arquivo na exibição de
    portas/portões trancados.

--- data/actions/scripts/item_desativado.lua
+++ data/actions/scripts/item_desativado.lua
@@ -1,3 +1,10 @@
 function onUse(player, item, fromPosition, itemEx, toPosition, isHotkey)
-	return false
+	local actionid = item:getActionId()
+	if(actionid == 2500) then
+		return false
+	elseif(actionid == 2501) then
+		return player:sendCancelMessage("Essa porta está trancada.")
+	elseif(actionid == 2502) then
+		return player:sendCancelMessage("Esse portão está trancado.")
+	end
 end
