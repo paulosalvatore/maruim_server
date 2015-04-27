9f9b7273a101fbc65950950d08e6d5d4dc302951
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-04-27 11:53:04 -0300

    Atualização da lib 'Actions'

--- data/actions/lib/actions.lua
+++ data/actions/lib/actions.lua
@@ -1,6 +1,7 @@
-function destroyItem(player, itemEx, toPosition)
-	if itemEx.actionid == 2902 and itemEx.itemid == 3422 then
-		itemEx:transform(3421)
+function destroyItem(player, target, toPosition)
+	local targetId = target:getId()
+	if target.actionid == 2902 and targetId == 3422 then
+		target:transform(3421)
 		toPosition:sendMagicEffect(CONST_ME_POFF)
 		addEvent(function(posicao)
 			local tile = Tile(posicao)
@@ -11,56 +12,56 @@ function destroyItem(player, itemEx, toPosition)
 		end, 60000, toPosition)
 		return true
 	end
-	if itemEx.uid <= 65535 or itemEx.actionid > 0 then
+	if target:hasAttribute(ITEM_ATTRIBUTE_UNIQUEID) or target:hasAttribute(ITEM_ATTRIBUTE_ACTIONID) then
 		return false
 	end
 	--chest,crate,barrel...
-	if (itemEx.itemid >= 1724 and itemEx.itemid <= 1741) or (itemEx.itemid >= 2581 and itemEx.itemid <= 2588) or itemEx.itemid == 1770 or itemEx.itemid == 2098 or itemEx.itemid == 1774 or itemEx.itemid == 1775 or itemEx.itemid == 2064 or (itemEx.itemid >= 1747 and itemEx.itemid <= 1753) or (itemEx.itemid >= 1714 and itemEx.itemid <= 1717) or (itemEx.itemid >= 1650 and itemEx.itemid <= 1653) or (itemEx.itemid >= 1666 and itemEx.itemid <= 1677) or (itemEx.itemid >= 1614 and itemEx.itemid <= 1616) or (itemEx.itemid >= 3813 and itemEx.itemid <= 3820) or (itemEx.itemid >= 3807 and itemEx.itemid <= 3810) or (itemEx.itemid >= 2080 and itemEx.itemid <= 2085) or (itemEx.itemid >= 2116 and itemEx.itemid <= 2119) or itemEx.itemid == 2094 or itemEx.itemid == 2095 or itemEx.itemid == 1619 or itemEx.itemid == 2602 or itemEx.itemid == 3805 or itemEx.itemid == 3806 then
+	if (targetId >= 1724 and targetId <= 1741) or (targetId >= 2581 and targetId <= 2588) or targetId == 1770 or targetId == 2098 or targetId == 1774 or targetId == 1775 or targetId == 2064 or (targetId >= 1747 and targetId <= 1753) or (targetId >= 1714 and targetId <= 1717) or (targetId >= 1650 and targetId <= 1653) or (targetId >= 1666 and targetId <= 1677) or (targetId >= 1614 and targetId <= 1616) or (targetId >= 3813 and targetId <= 3820) or (targetId >= 3807 and targetId <= 3810) or (targetId >= 2080 and targetId <= 2085) or (targetId >= 2116 and targetId <= 2119) or targetId == 2094 or targetId == 2095 or targetId == 1619 or targetId == 2602 or targetId == 3805 or targetId == 3806 then
 		if math.random(1, 7) == 1 then
-			if itemEx.itemid == 1738 or itemEx.itemid == 1739 or (itemEx.itemid >= 2581 and itemEx.itemid <= 2588) or itemEx.itemid == 1770 or itemEx.itemid == 2098 or itemEx.itemid == 1774 or itemEx.itemid == 1775 or itemEx.itemid == 2064 then
+			if targetId == 1738 or targetId == 1739 or (targetId >= 2581 and targetId <= 2588) or targetId == 1770 or targetId == 2098 or targetId == 1774 or targetId == 1775 or targetId == 2064 then
 				Game.createItem(2250, 1, toPosition)
-			elseif (itemEx.itemid >= 1747 and itemEx.itemid <= 1749) or itemEx.itemid == 1740 then
+			elseif (targetId >= 1747 and targetId <= 1749) or targetId == 1740 then
 				Game.createItem(2251, 1, toPosition)
-			elseif (itemEx.itemid >= 1714 and itemEx.itemid <= 1717) then
+			elseif (targetId >= 1714 and targetId <= 1717) then
 				Game.createItem(2252, 1, toPosition)
-			elseif (itemEx.itemid >= 1650 and itemEx.itemid <= 1653) or (itemEx.itemid >= 1666 and itemEx.itemid <= 1677) or (itemEx.itemid >= 1614 and itemEx.itemid <= 1616) or (itemEx.itemid >= 3813 and itemEx.itemid <= 3820) or (itemEx.itemid >= 3807 and itemEx.itemid <= 3810) then
+			elseif (targetId >= 1650 and targetId <= 1653) or (targetId >= 1666 and targetId <= 1677) or (targetId >= 1614 and targetId <= 1616) or (targetId >= 3813 and targetId <= 3820) or (targetId >= 3807 and targetId <= 3810) then
 				Game.createItem(2253, 1, toPosition)
-			elseif (itemEx.itemid >= 1724 and itemEx.itemid <= 1737) or (itemEx.itemid >= 2080 and itemEx.itemid <= 2085) or (itemEx.itemid >= 2116 and itemEx.itemid <= 2119) or itemEx.itemid == 2094 or itemEx.itemid == 2095 then
+			elseif (targetId >= 1724 and targetId <= 1737) or (targetId >= 2080 and targetId <= 2085) or (targetId >= 2116 and targetId <= 2119) or targetId == 2094 or targetId == 2095 then
 				Game.createItem(2254, 1, toPosition)
-			elseif (itemEx.itemid >= 1750 and itemEx.itemid <= 1753) or itemEx.itemid == 1619 or itemEx.itemid == 1741 then
+			elseif (targetId >= 1750 and targetId <= 1753) or targetId == 1619 or targetId == 1741 then
 				Game.createItem(2255, 1, toPosition)
-			elseif itemEx.itemid == 2602 then
+			elseif targetId == 2602 then
 				Game.createItem(2257, 1, toPosition)
-			elseif itemEx.itemid == 3805 or itemEx.itemid == 3806 then
+			elseif targetId == 3805 or targetId == 3806 then
 				Game.createItem(2259, 1, toPosition)
 			end
-			itemEx:remove(1)
+			target:remove(1)
 		end
 		toPosition:sendMagicEffect(CONST_ME_POFF)
 		return true
 	end
 	--spiderweb
-	if itemEx.itemid == 7538 or itemEx.itemid == 7539 then 
+	if targetId == 7538 or targetId == 7539 then 
 		if math.random(1, 7) == 1 then
-			if itemEx.itemid == 7538 then
-				itemEx:transform(7544)
-			elseif itemEx.itemid == 7539 then
-				itemEx:transform(7545)
+			if targetId == 7538 then
+				target:transform(7544)
+			elseif targetId == 7539 then
+				target:transform(7545)
 			end
-			itemEx:decay()
+			target:decay()
 		end
 		toPosition:sendMagicEffect(CONST_ME_POFF)
 		return true
 	end
 	--wooden bar
-	if itemEx.itemid == 3798 or itemEx.itemid == 3799 then 
+	if targetId == 3798 or targetId == 3799 then 
 		if math.random(1, 3) == 1 then
-			if itemEx.itemid == 3798 then
-				itemEx:transform(3959)
-			elseif itemEx.itemid == 3799 then
-				itemEx:transform(3958)
+			if targetId == 3798 then
+				target:transform(3959)
+			elseif targetId == 3799 then
+				target:transform(3958)
 			end
-			itemEx:decay()
+			target:decay()
 		end
 		toPosition:sendMagicEffect(CONST_ME_POFF)
 		return true
