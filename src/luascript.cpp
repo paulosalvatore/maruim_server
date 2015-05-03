fca29cdb3542f4586f23f7001be835859ff901c3
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-05-03 02:01:01 -0300

    Atualização sources

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -5079,9 +5079,9 @@ int LuaScriptInterface::luaTileGetGround(lua_State* L)
 {
 	// tile:getGround()
 	Tile* tile = getUserdata<Tile>(L, 1);
-	if (tile && tile->ground) {
-		pushUserdata<Item>(L, tile->ground);
-		setItemMetatable(L, -1, tile->ground);
+	if (tile && tile->getGround()) {
+		pushUserdata<Item>(L, tile->getGround());
+		setItemMetatable(L, -1, tile->getGround());
 	} else {
 		lua_pushnil(L);
 	}
@@ -5285,7 +5285,7 @@ int LuaScriptInterface::luaTileGetItemByType(lua_State* L)
 		return 1;
 	}
 
-	if (Item* item = tile->ground) {
+	if (Item* item = tile->getGround()) {
 		const ItemType& it = Item::items[item->getID()];
 		if (it.type == itemType) {
 			pushUserdata<Item>(L, item);
@@ -11096,7 +11096,7 @@ int LuaScriptInterface::luaItemTypeGetWeight(lua_State* L)
 		return 1;
 	}
 
-	uint64_t weight = (itemType->weight * std::max<int32_t>(1, count));
+	uint64_t weight = static_cast<uint64_t>(itemType->weight) * std::max<int32_t>(1, count);
 	lua_pushnumber(L, weight);
 	return 1;
 }
