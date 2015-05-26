1d283318b0111aa6675892163985608af0f8aca7
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-05-26 19:25:03 -0300

    Porta de Vocação adicionada

--- data/actions/scripts/other/doors.lua
+++ data/actions/scripts/other/doors.lua
@@ -1,11 +1,25 @@
+local vocationDoor = {
+	[905] = {1, 5},
+	[906] = {2, 6},
+	[907] = {3, 7},
+	[908] = {4, 8}
+}
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	local itemId = item:getId()
+	if vocationDoor[item.actionid] ~= nil then
+		if not isInArray(vocationDoor[item.actionid], player:getVocation():getId()) then
+			player:sendCancelMessage("Você precisa ser um " .. Vocation(vocationDoor[item.actionid][1]):getName() .. " para abrir essa porta.")
+			return true
+		end
+		item:transform(itemId + 1)
+		return true
+	end
 	if isInArray(questDoors, itemId) then
 		if player:getStorageValue(item.actionid) ~= -1 then
 			item:transform(itemId + 1)
 			player:teleportTo(toPosition, true)
 		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "Essa porta parece estar selada contra intrusos indesejados.")
 		end
 		return true
 	elseif isInArray(levelDoors, itemId) then
@@ -13,7 +27,7 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 			item:transform(itemId + 1)
 			player:teleportTo(toPosition, true)
 		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "Only the worthy may pass.")
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "Apenas os dignos podem passar.")
 		end
 		return true
 	elseif isInArray(keys, itemId) then
@@ -22,7 +36,7 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 				target:transform(doors[target.itemid])
 				return true
 			end
-			player:sendTextMessage(MESSAGE_STATUS_SMALL, "The key does not match.")
+			player:sendTextMessage(MESSAGE_STATUS_SMALL, "A chave não corresponde.")
 			return true
 		end
 		return false
@@ -57,7 +71,7 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		if item.actionid == 0 then
 			item:transform(doors[itemId])
 		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "It is locked.")
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "Está trancada.")
 		end
 		return true
 	end
