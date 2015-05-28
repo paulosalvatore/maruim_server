c3c948e1d67a544769d02ad0c11c9a5919e8bdfb
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-05-28 13:53:39 -0300

    Script dos NPC's do Templo finalizado
    NPC cura 10% do HP ou, caso o HP seja inferior a 400, cura 40 de HP e
    remove todos os efeitos negativos

--- data/npc/scripts/templo.lua
+++ data/npc/scripts/templo.lua
@@ -7,4 +7,25 @@ function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()				npcHandler:onThink()					end
 
+
+keywordHandler:addKeyword({{'heal'}, {'curar'}}, StdModule.say, {npcHandler = npcHandler, text = 'Você está ferida, criança. Eu irei curá-la.'},
+	function(player) return player:getHealth() < math.max(40, math.floor(player:getMaxHealth()*0.1)) end,
+	function(player)
+		local playerHP = player:getHealth()
+		local playerChecarHP = math.max(40, math.floor(player:getMaxHealth()*0.1))
+		local playerRecuperarHP = playerChecarHP-playerHP
+		if playerHP < playerChecarHP then player:addHealth(playerRecuperarHP) end
+		for i, v in ipairs(conditionsHealing) do
+			if(getCreatureCondition(player, v) == true) then
+				doRemoveCondition(player, v)
+			end
+		end
+		player:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
+	end
+)
+
+npcHandler:setMessage(MESSAGE_GREET, 'Seja bem-vindo, jovem |PLAYERNAME|! Se você está gravemente ferido ou envenenado, eu posso te {curar} sem cobrar nada.')
+npcHandler:setMessage(MESSAGE_WALKAWAY, 'Lembre-se: Se você está gravemente ferido ou envenenado, eu posso te {curar} sem cobrar nada.')
+npcHandler:setMessage(MESSAGE_FAREWELL, 'Que os Deuses estejam com você, |PLAYERNAME|!')
+
 npcHandler:addModule(FocusModule:new())
