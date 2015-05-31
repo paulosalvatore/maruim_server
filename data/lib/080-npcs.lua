d10362a64844df5817f804b2be3696519a00af5e
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-05-31 02:52:08 -0300

    Destinos das Viagens adicionados

--- data/lib/080-npcs.lua
+++ data/lib/080-npcs.lua
@@ -1,14 +1,69 @@
 destinos = {
 	["ilha_inicial"] = {
 		nome = "Ilha Inicial",
-		posicao = {x = 586, y = 920, z = 7},
+		posicao = {x = 644, y = 942, z = 6},
 		custo = 100,
-		levelNecessario = 502
+		levelNecessario = 0
 	},
-	["ilha_testes"] = {
-		nome = "Ilha de Testes",
-		posicao = {x = 856, y = 2091, z = 7},
-		custo = 100
+	["mugulu"] = {
+		nome = "Mugulu",
+		posicao = {x = 738, y = 1800, z = 6},
+		custo = 100,
+		levelNecessario = 0
+	},
+	["civitaten"] = {
+		nome = "Civitaten",
+		posicao = {x = 928, y = 1900, z = 2},
+		custo = 100,
+		levelNecessario = 0
+	},
+	["udecoat"] = {
+		nome = "Udecoat",
+		posicao = {x = 921, y = 1774, z = 8},
+		custo = 100,
+		levelNecessario = 0
+	},
+	["otto"] = {
+		nome = "Ôttô",
+		posicao = {x = 834, y = 1258, z = 6},
+		custo = 100,
+		levelNecessario = 0
+	},
+	["pundera"] = {
+		nome = "Pundera",
+		posicao = {x = 875, y = 1523, z = 7},
+		custo = 100,
+		levelNecessario = 0
+	},
+	["hyalakur"] = {
+		nome = "Hyalakur",
+		posicao = {x = 1084, y = 1592, z = 7},
+		custo = 100,
+		levelNecessario = 0
+	},
+	["lorn"] = {
+		nome = "Lorn",
+		posicao = {x = 1329, y = 1713, z = 6},
+		custo = 100,
+		levelNecessario = 0
+	},
+	["kyo"] = {
+		nome = "Kyo",
+		posicao = {x = 1397, y = 1871, z = 6},
+		custo = 100,
+		levelNecessario = 0
+	},
+	["algatar"] = {
+		nome = "Algatar",
+		posicao = {x = 1139, y = 1996, z = 6},
+		custo = 100,
+		levelNecessario = 0
+	},
+	["khazad_dum"] = {
+		nome = "Khazad-dûm",
+		posicao = {x = 1513, y = 1753, z = 8},
+		custo = 100,
+		levelNecessario = 0
 	}
 }
 barqueiros = {
