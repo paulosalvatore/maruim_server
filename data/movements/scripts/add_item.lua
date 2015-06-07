cd877a2c1c97f64b10677b3310f78cc5775d399d
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-07 17:19:46 -0300

    Modificações no script movements/add_item.lua
    Script modificado para transformar pieces of magic chal em shadow ashes

--- data/movements/scripts/add_item.lua
+++ data/movements/scripts/add_item.lua
@@ -35,16 +35,34 @@ local config = {
 			transform = {5812, 1},
 			efeito = "hit"
 		}
+	},
+	[21430] = {
+		[21247] = {
+			transform = {21431, 1},
+			transformMoveItem = {21446, 1},
+			efeito = "fire",
+			chanceSucesso = 9000
+		}
 	}
 }
 function onAddItem(moveItem, tileItem, position)
-	local itemConfig = config[tileItem.itemid][moveItem.itemid]
-	if itemConfig ~= nil then
+	if config[tileItem.itemid][moveItem.itemid] ~= nil then
+		local i = config[tileItem.itemid][moveItem.itemid]
+		local chance = math.random(1, 10000)
+		local chanceSucesso = 10000
+		if i.chanceSucesso ~= nil then
+			chanceSucesso = i.chanceSucesso
+		end
+		if chance <= chanceSucesso then
+			if i.transformMoveItem ~= nil then
+				Game.createItem(i.transformMoveItem[1], i.transformMoveItem[2], position)
+			end
+		end
 		Item(moveItem.uid):remove(1)
-		Item(tileItem.uid):transform(itemConfig.transform[1], itemConfig.transform[2])
+		Item(tileItem.uid):transform(i.transform[1], i.transform[2])
 		Item(tileItem.uid):decay()
-		if itemConfig.efeito ~= nil then
-			position:sendMagicEffect(efeitos[itemConfig.efeito])
+		if i.efeito ~= nil then
+			position:sendMagicEffect(efeitos[i.efeito])
 		end
 	end
 	return true
