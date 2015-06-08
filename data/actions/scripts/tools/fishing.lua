b439e77ca7c8fa0d2e20843f60a188f7d16b4fdf
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-08 01:39:54 -0300

    Modificações no script de fishing para shimmer

--- data/actions/scripts/tools/fishing.lua
+++ data/actions/scripts/tools/fishing.lua
@@ -1,4 +1,5 @@
 local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625, 4664, 4665, 4666, 7236, 10499, 15401, 15402}
+local shimmerWaterIds = {13547, 13548, 13549, 13550, 13551, 13552}
 local lootTrash = {2234, 2238, 2376, 2509, 2667}
 local lootCommon = {2152, 2167, 2168, 2669, 7588, 7589}
 local lootRare = {2143, 2146, 2149, 7158, 7159}
@@ -11,80 +12,93 @@ local conditionEnergy = createConditionObject(CONDITION_ENERGY)
 addDamageCondition(conditionEnergy, 1, 1000, -35)
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	local targetId = target.itemid
-	if not isInArray(waterIds, targetId) then
+	if not isInArray(waterIds, targetId) or not isInArray(shimmerWaterIds, targetId) then
 		return false
 	end
-	if item.itemid == 2580 then
-		if targetId == 10499 then
-			local owner = target:getAttribute(ITEM_ATTRIBUTE_CORPSEOWNER)
-			if owner ~= 0 and owner ~= player:getId() then
-				player:sendTextMessage(MESSAGE_STATUS_SMALL, "You are not the owner.")
+	if isInArray(waterIds, targetId) then
+		if item.itemid == 2580 then
+			if targetId == 10499 then
+				local owner = target:getAttribute(ITEM_ATTRIBUTE_CORPSEOWNER)
+				if owner ~= 0 and owner ~= player:getId() then
+					player:sendTextMessage(MESSAGE_STATUS_SMALL, "You are not the owner.")
+					return true
+				end
+				toPosition:sendMagicEffect(CONST_ME_WATERSPLASH)
+				target:remove()
+				local rareChance = math.random(1, 100)
+				if rareChance == 1 then
+					player:addItem(lootVeryRare[math.random(#lootVeryRare)], 1)
+				elseif rareChance <= 3 then
+					player:addItem(lootRare[math.random(#lootRare)], 1)
+				elseif rareChance <= 10 then
+					player:addItem(lootCommon[math.random(#lootCommon)], 1)
+				else
+					player:addItem(lootTrash[math.random(#lootTrash)], 1)
+				end
+				return true
+			end
+			if targetId ~= 7236 then
+				toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
+			end
+			if targetId == 493 or targetId == 15402 then
+				return true
+			end
+			player:addSkillTries(SKILL_FISHING, 1)
+			if math.random(1, 100) <= math.min(math.max(10 + (player:getEffectiveSkillLevel(SKILL_FISHING) - 10) * 0.597, 10), 50) then
+				if useWorms and not player:removeItem("worm", 1) then
+					return true
+				end
+				if targetId == 15401 then
+					local target = Item(target.uid)
+					target:transform(targetId + 1)
+					target:decay()
+					if math.random(1, 100) >= 97 then
+						player:addItem(15405, 1)
+						return true
+					end
+				elseif targetId == 7236 then
+					local target = Item(target.uid)
+					target:transform(targetId + 1)
+					target:decay()
+					local rareChance = math.random(1, 100)
+					if rareChance == 1 then
+						player:addItem(7158, 1)
+						return true
+					elseif rareChance <= 4 then
+						player:addItem(2669, 1)
+						return true
+					elseif rareChance <= 10 then
+						player:addItem(7159, 1)
+						return true
+					end
+				end
+				player:addItem("fish", 1)
+			end
+		elseif item.itemid == 10223 then
+			if not player:removeItem(8309, 1) then
 				return true
 			end
 			toPosition:sendMagicEffect(CONST_ME_WATERSPLASH)
-			target:remove()
-			local rareChance = math.random(1, 100)
-			if rareChance == 1 then
-				player:addItem(lootVeryRare[math.random(#lootVeryRare)], 1)
-			elseif rareChance <= 3 then
-				player:addItem(lootRare[math.random(#lootRare)], 1)
-			elseif rareChance <= 10 then
-				player:addItem(lootCommon[math.random(#lootCommon)], 1)
-			else
-				player:addItem(lootTrash[math.random(#lootTrash)], 1)
+			local chance = math.random(1, 100)
+			if chance <= 10 then
+				player:addItem(10224, 1)
+				player:sendTextMessage(MESSAGE_EVENT_ORANGE, '"You have caught one of the elusive mechanical fishes!"')
+			elseif chance >= 85 then
+				player:sendTextMessage(MESSAGE_EVENT_ORANGE, '"Yikes, That DID hurt!"')
+				doAddCondition(player, conditionOutfit)
+				doAddCondition(player, conditionEnergy)
 			end
-			return true
 		end
-		if targetId ~= 7236 then
-			toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
-		end
-		if targetId == 493 or targetId == 15402 then
-			return true
-		end
-		player:addSkillTries(SKILL_FISHING, 1)
-		if math.random(1, 100) <= math.min(math.max(10 + (player:getEffectiveSkillLevel(SKILL_FISHING) - 10) * 0.597, 10), 50) then
+	elseif isInArray(shimmerWaterIds, targetId) then
+		if item.itemid == 2580 then
+			player:addSkillTries(SKILL_FISHING, 1)
 			if useWorms and not player:removeItem("worm", 1) then
 				return true
 			end
-			if targetId == 15401 then
-				local target = Item(target.uid)
-				target:transform(targetId + 1)
-				target:decay()
-				if math.random(1, 100) >= 97 then
-					player:addItem(15405, 1)
-					return true
-				end
-			elseif targetId == 7236 then
-				local target = Item(target.uid)
-				target:transform(targetId + 1)
-				target:decay()
-				local rareChance = math.random(1, 100)
-				if rareChance == 1 then
-					player:addItem(7158, 1)
-					return true
-				elseif rareChance <= 4 then
-					player:addItem(2669, 1)
-					return true
-				elseif rareChance <= 10 then
-					player:addItem(7159, 1)
-					return true
-				end
+			-- if math.random(1, 100) <= math.min(math.max(10 + (player:getEffectiveSkillLevel(SKILL_FISHING) - 10) * 0.597, 10), 50) then
+			if math.random(1, 100) <= 5 then
+				player:addItem(13546, 1)
 			end
-			player:addItem("fish", 1)
-		end
-	elseif item.itemid == 10223 then
-		if not player:removeItem(8309, 1) then
-			return true
-		end
-		toPosition:sendMagicEffect(CONST_ME_WATERSPLASH)
-		local chance = math.random(1, 100)
-		if chance <= 10 then
-			player:addItem(10224, 1)
-			player:sendTextMessage(MESSAGE_EVENT_ORANGE, '"You have caught one of the elusive mechanical fishes!"')
-		elseif chance >= 85 then
-			player:sendTextMessage(MESSAGE_EVENT_ORANGE, '"Yikes, That DID hurt!"')
-			doAddCondition(player, conditionOutfit)
-			doAddCondition(player, conditionEnergy)
 		end
 	end
 	return true
