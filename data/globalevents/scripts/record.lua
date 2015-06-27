a40d1d4ab494a196cc8d60bcb02e8194c37500b5
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-26 23:33:46 -0300

    Adicionado o Horário do Recorde de Jogadores

--- data/globalevents/scripts/record.lua
+++ data/globalevents/scripts/record.lua
@@ -1,4 +1,14 @@
 function onRecord(current, old)
-	addEvent(broadcastMessage, 150, "New record: " .. current .. " players are logged in.", MESSAGE_STATUS_DEFAULT)
+	local resultId = db.storeQuery("SELECT `config` FROM `server_config` WHERE `config` = 'players_record_time'")
+	if resultId == false then
+		db.query("INSERT INTO `server_config` (`config`, `value`) VALUES ('players_record_time', " .. db.escapeString(os.time()) .. ")")
+	else
+		db.query("UPDATE `server_config` SET `value` = " .. db.escapeString(os.time()) .. " WHERE `config` = 'players_record_time'")
+	end
+	local player = "players"
+	if current == 1 then
+		player = "player"
+	end
+	addEvent(broadcastMessage, 150, "New record: " .. current .. " " .. player .. " are logged in.", MESSAGE_STATUS_DEFAULT)
 	return true
 end
