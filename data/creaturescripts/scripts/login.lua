1a5384c1a3ef84ea3e97a6cf01e7574146061565
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-30 11:18:19 -0300

    Mensagems do Login traduzidas

--- data/creaturescripts/scripts/login.lua
+++ data/creaturescripts/scripts/login.lua
@@ -1,14 +1,14 @@
 function onLogin(player)
-	local loginStr = "Welcome to " .. configManager.getString(configKeys.SERVER_NAME) .. "!"
+	local loginStr = "Seja bem-vindo ao " .. configManager.getString(configKeys.SERVER_NAME) .. "!"
 	if player:getLastLoginSaved() <= 0 then
-		loginStr = loginStr .. " Please choose your outfit."
+		loginStr = loginStr .. " Por favor, escolha seu outfit."
 		player:sendOutfitWindow()
 	else
 		if loginStr ~= "" then
 			player:sendTextMessage(MESSAGE_STATUS_DEFAULT, loginStr)
 		end
 
-		loginStr = string.format("Your last visit was on %s.", os.date("%a %b %d %X %Y", player:getLastLoginSaved()))
+		loginStr = string.format("Sua última visita foi em %s.", os.date("%a %b %d %X %Y", player:getLastLoginSaved()))
 	end
 	player:sendTextMessage(MESSAGE_STATUS_DEFAULT, loginStr)
 
