dd312950f406cc5c62259c032710c9811f16b9f0
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-06-30 19:10:10 -0300

    Modificações no script 'logout.lua'
    Jogador não pode se desconectar enquanto está fabricando algo.

--- data/creaturescripts/scripts/logout.lua
+++ data/creaturescripts/scripts/logout.lua
@@ -1,5 +1,10 @@
 function onLogout(player)
 
+	if getCreatureCondition(player, CONDITION_SPELLCOOLDOWN, 160) then
+		player:sendCancelMessage("Você não pode desconectar-se agora.")
+		return false
+	end
+
 	if player:verificarMasmorra() > 0 then
 		player:sendCancelMessage(configMasmorras.mensagens.logoutBloqueado)
 		return false
