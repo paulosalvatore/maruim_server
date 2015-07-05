40fe74053c39f6b2fbccaa632716dbf9d0184678
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-07-05 17:21:45 -0300

    Limite de Pacotes aumentado

--- config.lua
+++ config.lua
@@ -27,7 +27,7 @@ allowClones = false
 serverName = "MaruimOT"
 statusTimeout = 5000
 replaceKickOnLogin = true
-maxPacketsPerSecond = 25
+maxPacketsPerSecond = 40
 
 -- Deaths
 -- NOTE: Leave deathLosePercent as -1 if you want to use the default
