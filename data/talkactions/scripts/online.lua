28827e60f525ca09ef51f7178340a3c8dd5f75a7
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-07-07 18:46:16 -0300

    Comando /online adicionado

--- data/talkactions/scripts/online.lua
+++ data/talkactions/scripts/online.lua
@@ -1,4 +1,12 @@
 function onSay(player, words, param)
+	if not player:getGroup():getAccess() then
+		return true
+	end
+
+	if player:getAccountType() < ACCOUNT_TYPE_GOD then
+		return false
+	end
+
 	local hasAccess = player:getGroup():getAccess()
 	local players = Game.getPlayers()
 	local playerCount = Game.getPlayerCount()
