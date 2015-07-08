14ddddc00a36c1bdca59cba43e59ddd6b6376757
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-07-08 14:06:14 -0300

    Correção na função de report
    Pegando a data correta

--- src/game.cpp
+++ src/game.cpp
@@ -4849,7 +4849,7 @@ void Game::playerReportBug(uint32_t playerId, const std::string& message, const 
 	}
 	Database* db = Database::getInstance();
 	std::ostringstream query;
-	uint64_t currentTime = OTSYS_TIME();
+	uint64_t currentTime = time(nullptr);
 	query << "INSERT INTO `reports` (`conta`, `posicao_x`, `posicao_y`, `posicao_z`, `mensagem`, `categoria`, `data`) VALUES (" << player->getAccount() << ',' << position.getX() << ',' << position.getY() << ',' << position.getZ() << ',' << db->escapeString(message.c_str()) << ',' << db->escapeString(showCategory.c_str()) << ',' << currentTime << ')';
 	db->executeQuery(query.str());
 
