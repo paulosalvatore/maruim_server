359f3cf78e2200700f364ebaebc10d62f8e7c469
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-07-20 01:26:35 -0300

    Melhorias no NPC Flora

--- data/npc/scripts/Flora.lua
+++ data/npc/scripts/Flora.lua
@@ -5,12 +5,25 @@ NpcSystem.parseParameters(npcHandler)
 local shopModule = ShopModule:new()
 npcHandler:addModule(shopModule)
 
+local quantidadePocoes = {}
+
 function onCreatureAppear(player)       npcHandler:onCreatureAppear(player)       end
 function onCreatureDisappear(player)     npcHandler:onCreatureDisappear(player)       end
 function onCreatureSay(player, type, msg)	npcHandler:onCreatureSay(player, type, msg)	end
 function onThink()         npcHandler:onThink()           end
 function onPlayerEndTrade(cid)				npcHandler:onPlayerEndTrade(cid)			end
 function onPlayerCloseChannel(cid)			npcHandler:onPlayerCloseChannel(cid)		end
+
+local function exibirQuantidadePocaoDisponivel(quantidade, marcarNumero)
+	local exibirQuantidadePocaoDisponivel = "ão"
+	if quantidade > 1 then
+		exibirQuantidadePocaoDisponivel = "ões"
+	end
+	if marcarNumero then
+		quantidade = "{" .. quantidade .. "}"
+	end
+	return quantidade .. " poç" .. exibirQuantidadePocaoDisponivel
+end
 
 local function creatureSayCallback(cid, type, msg)
 	if not npcHandler:isFocused(cid) then
@@ -23,35 +36,41 @@ local function creatureSayCallback(cid, type, msg)
 	local quantidadeErva = player:getItemCount(ervaId)
 	if isInArray({"potion", "pocao", "poção", "pocoes", "poções"}, msg) then
 		if quantidadeErva > 0 then
-			local exibirPocaoDisponivel = "ão"
-			if quantidadeErva > 1 then
-				exibirPocaoDisponivel = "ões"
-			end
-			npcHandler:say("Você pode fazer até " .. quantidadeErva .. " poç" .. exibirPocaoDisponivel .. ". Você quer fazer uma quantidade específica ou deseja fazer {tudo}?", cid)
+			npcHandler:say("Você pode fazer até " .. exibirQuantidadePocaoDisponivel(quantidadeErva, true) .. ". Você quer fazer uma {quantidade} específica ou deseja fazer {tudo}?", cid)
 			npcHandler.topic[cid] = 1
 		else
 			npcHandler:say("Você não possui ervas disponíveis.", cid)
+			npcHandler.topic[cid] = 0
 		end
 	elseif npcHandler.topic[cid] == 1 then
-		local quantidadePocoes = 0
 		if isInArray({"all", "todas", "todos", "tudo"}, msg) then
-			quantidadePocoes = quantidadeErva
+			quantidadePocoes[cid] = quantidadeErva
 		elseif tonumber(msg) ~= nil then
-			quantidadePocoes = tonumber(msg)
+			quantidadePocoes[cid] = tonumber(msg)
 		end
-		if quantidadePocoes > 0 then
-			if player:removeItem(ervaId, quantidadePocoes) then
-				player:addItem(pocaoId, quantidadePocoes)
-				npcHandler:say("Aqui está!", cid)
+		if quantidadePocoes[cid] > 0 then
+			npcHandler:say("Você deseja fazer " .. exibirQuantidadePocaoDisponivel(quantidadePocoes[cid]) .. "?", cid)
+			npcHandler.topic[cid] = 2
+		end
+	elseif npcHandler.topic[cid] == 2 then
+		if isInArray({"yes", "sim"}, msg) then
+			if player:removeItem(ervaId, quantidadePocoes[cid]) then
+				player:addItem(pocaoId, quantidadePocoes[cid])
+				npcHandler:say("Prontinho! Faça um bom uso!", cid)
 				npcHandler.topic[cid] = 0
 			else
 				npcHandler:say("Você não possui ervas suficiente.", cid)
+				npcHandler.topic[cid] = 0
 			end
+		elseif isInArray({"no", "não", "nao"}, msg) then
+			npcHandler:say("Tudo bem. Quando quiser fazer {poções} volte a falar comigo!", cid)
+			npcHandler.topic[cid] = 1
 		end
 	end
 end
 
 keywordHandler:addKeywords({{'erva'}, {'heaven blossom'}}, StdModule.say, {npcHandler = npcHandler, text = "A heaven blossom é uma erva excelente na criação de {poções} de cura. Você pode coletá-la em diversos locais da Maruim Island, basta selecionar a opção 'use' em cima de uma que esteja brilhando."})
+keywordHandler:addKeyword({'quantidade'}, StdModule.say, {npcHandler = npcHandler, text = "Quantas poções deseja fazer?"})
 
 npcHandler:setMessage(MESSAGE_GREET, "Olá |PLAYERNAME|. Eu posso fazer {poções} para você. Basta me trazer uma {erva} chamada {heaven blossom}.")
 npcHandler:setMessage(MESSAGE_FAREWELL, "Até logo!")
