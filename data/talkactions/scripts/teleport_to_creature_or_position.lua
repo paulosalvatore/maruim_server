4ae9910468142165567d7083bb4f12d43e180557
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-07-21 01:23:32 -0300

    Modificação no comendo /goto
    Comando agora aceita parâmetros de posição fornecidos pelo Map Editor
    através da opção 'Copy Position'. Ex.: {x = 0000, y = 0000, z = 0}

--- data/talkactions/scripts/teleport_to_creature_or_position.lua
+++ data/talkactions/scripts/teleport_to_creature_or_position.lua
@@ -6,17 +6,24 @@ function onSay(player, words, param)
 	local target = Creature(param)
 	if target == nil then
 		local split = param:split(",")
-		if	split[1] == nil or split[2] == nil or split[3] == nil then
-			player:sendCancelMessage("Insufficient parameters.")
+		if split[1] == nil or split[2] == nil or split[3] == nil then
+			player:sendCancelMessage("Parâmetros insuficientes.")
 			return false
 		else
+			-- {x = 1150, y = 1695, z = 5}
+			if split[1]:find("x") and split[2]:find("y") and split[3]:find("z") then
+				split[1] = split[1]:gsub("{x = ", "")
+				split[2] = split[2]:gsub("y = ", "")
+				split[3] = split[3]:gsub("}", "")
+				split[3] = split[3]:gsub("z = ", "")
+			end
 			if not player:teleportTo(Position(split[1], split[2], split[3])) then
-				player:sendCancelMessage("Position not found.")
+				player:sendCancelMessage("Posição não encontrada.")
 			end
 			return false
 		end
 
-		player:sendCancelMessage("Creature not found.")
+		player:sendCancelMessage("Criatura não encontrada.")
 		return false
 	end
 
