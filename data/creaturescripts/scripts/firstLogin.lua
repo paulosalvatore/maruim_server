3ee3ce4836327425616975cfcad2c64da6254acd
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-12-17 17:42:57 -0200

    Script 'firstLogin' modificado
    Itens que antes eram obtidos no tutorial serão adicionados direto
    (equipamentos e armas)
    Função de chamada Janela Modal das armas dos Knights foi movida direto
    para o script (antes ficava no script de tutorial)
    Pequena otimização realizada

--- data/creaturescripts/scripts/firstLogin.lua
+++ data/creaturescripts/scripts/firstLogin.lua
@@ -6,20 +6,51 @@ function onLogin(player)
 				player:addOutfitAddon(i, 2)
 			end
 		end
+
 		for i = 1, 80 do
 			player:addMount(i)
 		end
+
+		player:addItem(2461)
+		player:addItem(2467)
+		player:addItem(2649)
+
+		local vocacaoJogador = player:getVocation():getId()
+		if vocacaoJogador == 1 then
+			player:addItem(23719, 1)
+		elseif vocacaoJogador == 2 then
+			player:addItem(23721, 1)
+		elseif vocacaoJogador == 3 then
+			player:addItem(19390, 10)
+		end
+
 		player:addItem(2050, 1)
 		player:addItem(1988, 1):addItem(2789, 10)
+
 		local item = player:addItem(3901, 1)
 		item:setAttribute(ITEM_ATTRIBUTE_NAME, "rope and shovel kit")
 		item:setAttribute(ITEM_ATTRIBUTE_WEIGHT, 100)
 		item:setAttribute(ITEM_ATTRIBUTE_DESCRIPTION, "Use it only when you need a rope or a shovel.")
 		item:setActionId(3901)
+
 		player:sendOutfitWindow()
 	end
-	if player:getVocation():getId() == 4 and player:pegarPassoTutorial() == tutorialFinalizado and player:getStorageValue(Storage.itensKnight) ~= 1 then
-		player:enviarModalItensKnight()
+
+	if player:getVocation():getId() == 4 and player:getStorageValue(Storage.itensKnight) ~= 1 then
+		local modalTitulo = "Escolha uma Arma"
+		local modalMensagem = "Escolha uma das armas abaixo e clique em 'Escolher', tecle entre ou dê um clique duplo na opção desejada para recebê-la.\n\n"
+		local modal = ModalWindow(modalItensKnight, modalTitulo, modalMensagem)
+		modal:addChoice(1, "Arma Aleatória")
+		modal:addChoice(2, capAll(ItemType(itensKnight[1]):getName()) .. " (Espada)")
+		modal:addChoice(3, capAll(ItemType(itensKnight[2]):getName()) .. " (Clava)")
+		modal:addChoice(4, capAll(ItemType(itensKnight[3]):getName()) .. " (Machado)")
+		modal:addButton(1, "Escolher")
+		modal:setDefaultEnterButton(1)
+		modal:addButton(2, "Sair")
+		modal:setDefaultEscapeButton(2)
+		modal:sendToPlayer(player)
+		player:registerEvent("ItensKnight")
 	end
+
 	return true
 end
