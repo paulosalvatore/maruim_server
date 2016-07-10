0233015fc1c65dcf62c7b9525a8cd625de26fd79
paulosalvatore <paulo.salvatore@hotmail.com.br>
2016-07-10 04:15:20 -0300

    Correções de BUGs no script de 'LevelUp'
    ID do Modal Informativo de Promotion estava errada.
    Removendo itens iniciais ao atingir o nível 30
    Correção do bug de recompensa por nível
    Pequenas correções no modal informativo do nível 10

--- data/creaturescripts/scripts/LevelUp.lua
+++ data/creaturescripts/scripts/LevelUp.lua
@@ -11,11 +11,11 @@ function onAdvance(player, skill, oldlevel, newlevel)
 			modalMensagem = modalMensagem .. "necessários para a sua promoção no nível 20.\n\nAlgumas "
 			modalMensagem = modalMensagem .. "tarefas são bem fáceis de fazer e dão exatamente a "
 			modalMensagem = modalMensagem .. "quantidade de pontos que você precisa.\n\nSe quiser "
-			modalMensagem = modalMensagem .. "treinar mais aqui, você podecontinuar nessa ilha por "
-			modalMensagem = modalMensagem .. "quanto tempo quiser e semprepoderá viajar de volta.\n\n"
+			modalMensagem = modalMensagem .. "treinar mais aqui, você pode continuar nessa ilha por "
+			modalMensagem = modalMensagem .. "quanto tempo quiser e sempre poderá viajar de volta.\n\n"
 			modalMensagem = modalMensagem .. "Fique à vontade para explorar, existem diversas áreas de "
 			modalMensagem = modalMensagem .. "caça espalhadas pela ilha. Se quiser sair e viajar para o "
-			modalMensagem = modalMensagem .. "continente principal dirija-se ao Navio localizadono leste "
+			modalMensagem = modalMensagem .. "continente principal dirija-se ao Navio localizado ao leste "
 			modalMensagem = modalMensagem .. "da ilha."
 			local modal = ModalWindow(modalId, modalTitulo, modalMensagem)
 			modal:addButton(1, "Ok")
@@ -25,10 +25,8 @@ function onAdvance(player, skill, oldlevel, newlevel)
 			modal:sendToPlayer(player)
 			modalRecompensaAberto[player:getId()] = 0
 			player:registerEvent("ModalInformativo")
-		end
-
-		if newlevel == 20 then
-			local modalPromotion = 5200
+		elseif newlevel == 20 then
+			local modalPromotion = 5202
 			local modalTitulo = "Promova a sua Vocação!"
 			local modalMensagem = "Você já possui level para receber uma promoção!\n\nAssim que conseguir " .. Reputacao.promocao .. " pontos de reputação fale com o mestre da sua guilda, '" .. pegarNpcReputacao(player:getVocation():getId()) .. "', e pergunte a ele sobre promoção.\n\nVocê pode conseguir pontos de reputação após concluir algumas tarefas."
 			local modal = ModalWindow(modalPromotion, modalTitulo, modalMensagem)
@@ -39,16 +37,30 @@ function onAdvance(player, skill, oldlevel, newlevel)
 			modal:sendToPlayer(player)
 			modalRecompensaAberto[player:getId()] = 0
 			player:registerEvent("ModalInformativo")
+		elseif newlevel == 30 then
+			local pergaminhoId, pergaminhoAction = 1948, 3902
+			local refilId, refilAction = 1949, 3903
+
+			local pergaminho = player:getAllItemsByAction(pergaminhoId, pergaminhoAction)
+			local refil = player:getAllItemsByAction(refilId, refilAction)
+
+			if #pergaminho == 1 and #refil == 1 then
+				pergaminho[1]:remove(1)
+				refil[1]:remove(1)
+				player:sendTextMessage(MESSAGE_INFO_DESCR, "Você chegou ao nível 30. Seu pergaminho de teleporte e refil de poções foram removidos.")
+			end
 		end
 
 		eventoAdicionado = false
 		for a, b in pairs(RecompensasNivel) do
 			if b.nivel == newlevel then
 				if b.vocacao == nil or (b.vocacao ~= nil and isInArray(b.vocacao, string.lower(player:getVocation():getName()))) then
-					player:adicionarRecompensaPendente(a)
-					if not eventoAdicionado then
-						player:registerEvent("RecompensaNivel")
-						eventoAdicionado = true
+					if not player:checarRecompensaFinalizada(a) then
+						player:adicionarRecompensaPendente(a)
+						if not eventoAdicionado then
+							player:registerEvent("RecompensaNivel")
+							eventoAdicionado = true
+						end
 					end
 				end
 			end
