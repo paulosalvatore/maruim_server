8df33b14d5c45415379db40373f15da24449e22e
paulosalvatore <paulo.salvatore@hotmail.com.br>
2016-07-12 12:10:04 -0300

    Correção no script dos itens desativados

--- data/actions/scripts/item_desativado.lua
+++ data/actions/scripts/item_desativado.lua
@@ -1,10 +1,10 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	local actionid = item:getActionId()
-	if(actionid == 2500) then
-		return false
-	elseif(actionid == 2501) then
+	local actionId = item:getActionId()
+	if actionId == 2500 then
+		return player:sendCancelMessage("You cannot use this object.")
+	elseif actionId == 2501 then
 		return player:sendTextMessage(MESSAGE_INFO_DESCR, "Essa porta está trancada.")
-	elseif(actionid == 2502) then
+	elseif actionId == 2502 then
 		return player:sendTextMessage(MESSAGE_INFO_DESCR, "Esse portão está trancado.")
 	end
 end
