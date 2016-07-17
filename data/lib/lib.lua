4ebb00729059513f9285abf3248c0588baa54065
paulosalvatore <paulo.salvatore@hotmail.com.br>
2016-07-17 18:10:52 -0300

    Includes de arquivos da lib modificados

--- data/lib/lib.lua
+++ data/lib/lib.lua
@@ -3,3 +3,19 @@ dofile('data/lib/core/core.lua')
 
 -- Compatibility library for our old Lua API
 dofile('data/lib/compat/compat.lua')
+
+-- Libs personalizadas
+dofile('data/lib/050-functions.lua')
+dofile('data/lib/051-storages.lua')
+dofile('data/lib/080-npcs.lua')
+dofile('data/lib/vocacoes.lua')
+dofile('data/lib/itens.lua')
+dofile('data/lib/090-reputacao.lua')
+dofile('data/lib/091-tasks.lua')
+dofile('data/lib/tutorial.lua')
+dofile('data/lib/marcasMapa.lua')
+dofile('data/lib/recompensaNivel.lua')
+dofile('data/lib/carregarMiniMapa.lua')
+dofile('data/lib/casamento.lua')
+dofile('data/lib/monstros.lua')
+dofile('data/lib/rewardboss.lua')
