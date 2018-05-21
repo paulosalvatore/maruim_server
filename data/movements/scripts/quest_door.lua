624be4e985b76ecbd0ef11692ca248b1ebb6388c
Paulo Salvatore <salvatoregames@gmail.com>
2018-05-20 21:36:15 -0300

    Correção temporária na quest do Horestis

--- data/movements/scripts/quest_door.lua
+++ data/movements/scripts/quest_door.lua
@@ -3,8 +3,10 @@ function onStepIn(creature, item, position, fromPosition)
 	if player == nil then
 		return false
 	end
+	
+	local ignorar = {3303}
 
-	if player:getStorageValue(item.actionid) == -1 then
+	if not isInArray(ignorar, item.actionid) and player:getStorageValue(item.actionid) == -1 then
 		player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
 		player:teleportTo(fromPosition, true)
 		return false
